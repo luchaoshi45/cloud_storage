0acce14e575600ac8a9470c99bc917b8c0b35d9d
user <635683261@qq.com>
2024-04-28 18:50:04 +0800

    router 单例

--- router/router.go
+++ router/router.go
@@ -3,26 +3,39 @@ package router
 import (
 	"cloud_storage/handler"
 	"net/http"
+	"sync"
 )
 
+// handlerFunc
 type handlerFunc func(http.ResponseWriter, *http.Request)
 
-// RouterDict 路由字典
-var RouterDict = make(map[string]handlerFunc)
+// routerDict 路由字典
+var routerDict map[string]handlerFunc
+var once sync.Once
+
+// GetRouterDict 获得 RouterDict 路由字典
+func GetRouterDict() map[string]handlerFunc {
+	once.Do(func() {
+		routerDict = make(map[string]handlerFunc)
+	})
+	return routerDict
+}
 
 func Router() {
+	GetRouterDict()
 	addEntry("/file/upload", handler.NewUploadHandler().Handler)
 	addEntry("/file/upload/success", handler.NewUploadSuccessHandler().Handler)
 	config()
 }
 
-// 向全局字典中添加新的条目
+// 向 routerDict 中添加新的条目
 func addEntry(key string, value handlerFunc) {
-	RouterDict[key] = value
+	routerDict[key] = value
 }
 
+// 配置路由
 func config() {
-	for k, v := range RouterDict {
+	for k, v := range routerDict {
 		http.HandleFunc(k, v)
 	}
 }
